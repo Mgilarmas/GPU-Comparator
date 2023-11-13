1d5e1852a3e624ffcf51e3e4457dc449ae2b33d9
Mauricio <mauriciogilarmas@gmail.com>
2023-11-13 19:44:35 +0000

    Adding some new effects

--- js/parallax.js
+++ js/parallax.js
@@ -5,7 +5,7 @@ function initialize() {
   function moveImagesWhenUserScrolls(){
   const SCROLL_POSITION = window.scrollY;
   
-    const RTX = document.getElementById("rtx-2080");
+    const RTX = document.getElementById("electronic-background");
     RTX.style.left = SCROLL_POSITION + "px";
   }
   
