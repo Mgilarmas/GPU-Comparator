537ff8d2d1b113b839b8d4a7c65934819a2d906c
Mauricio <mauriciogilarmas@gmail.com>
2023-12-14 18:13:31 +0000

    adding format

--- js/firebase-chat.js
+++ js/firebase-chat.js
@@ -6,57 +6,57 @@ import { getDatabase, ref, onValue, push, set } from "https://www.gstatic.com/fi
 
 // Your web app's Firebase configuration
 const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyAxJizboBRVbePXqXjv-ZRa6LtA197BvkA",
-    authDomain: "gpufire.firebaseapp.com",
-    databaseURL: "https://gpufire-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "gpufire",
-    storageBucket: "gpufire.appspot.com",
-    messagingSenderId: "547079974685",
-    appId: "1:547079974685:web:4ab2e552d36af1153f7bf4"
-  };
+  apiKey: "AIzaSyAxJizboBRVbePXqXjv-ZRa6LtA197BvkA",
+  authDomain: "gpufire.firebaseapp.com",
+  databaseURL: "https://gpufire-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "gpufire",
+  storageBucket: "gpufire.appspot.com",
+  messagingSenderId: "547079974685",
+  appId: "1:547079974685:web:4ab2e552d36af1153f7bf4"
+};
 
 // Initialize Firebase
 initializeApp(FIREBASE_CONFIG);
 
 function initialize() {
-    const FORM_MESSAGE = document.querySelector(".chat");
-    FORM_MESSAGE.addEventListener("submit", addMessage);
-    showMessages();
+  const FORM_MESSAGE = document.querySelector(".chat");
+  FORM_MESSAGE.addEventListener("submit", addMessage);
+  showMessages();
 }
 
 function showMessages() {
-    const MESSAGES_REF = ref(getDatabase(), "messages/");
+  const MESSAGES_REF = ref(getDatabase(), "messages/");
 
-    onValue(MESSAGES_REF, showAllMessages);
+  onValue(MESSAGES_REF, showAllMessages);
 }
 
 function showAllMessages(snapshot) {
-    const VALUES = snapshot.val();
+  const VALUES = snapshot.val();
 
-    const SENT_MESSAGES = document.getElementById("message-list");
-    SENT_MESSAGES.innerHTML = "";
+  const SENT_MESSAGES = document.getElementById("message-list");
+  SENT_MESSAGES.innerHTML = "";
 
-    for (let value in VALUES) {
-        const SENDER = VALUES[value].sender;
-        const VALUE = VALUES[value].text;
+  for (let value in VALUES) {
+    const SENDER = VALUES[value].sender;
+    const VALUE = VALUES[value].text;
 
 
-        SENT_MESSAGES.innerHTML += `<p>${SENDER}: ${VALUE}</p>`
-    }
+    SENT_MESSAGES.innerHTML += `<p>${SENDER}: ${VALUE}</p>`
+  }
 }
 
 function addMessage(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const MESSAGE_SENDER = e.target["message-sender"].value;
-    const MESSAGE_TEXT = e.target["message-text"].value;
+  const MESSAGE_SENDER = e.target["message-sender"].value;
+  const MESSAGE_TEXT = e.target["message-text"].value;
 
-    const MESSAGES_REF = ref(getDatabase(), "messages/");
-    const NEW_MESSAGE = {
-        sender: MESSAGE_SENDER,
-        text: MESSAGE_TEXT,
-    }
-    push(MESSAGES_REF, NEW_MESSAGE);
+  const MESSAGES_REF = ref(getDatabase(), "messages/");
+  const NEW_MESSAGE = {
+    sender: MESSAGE_SENDER,
+    text: MESSAGE_TEXT,
+  }
+  push(MESSAGES_REF, NEW_MESSAGE);
 }
 
 initialize();
